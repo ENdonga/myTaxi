85fd86b170ae71133cd31c9505dd7bf97b829f1f
endonga <ndonga.edwin@gmail.com>
2018-04-04 15:26:32 +0300

    chnaged folder

--- app/src/androidTest/java/com/mytaxi/android_demo/LoginTest.java
+++ app/src/androidTest/java/com/mytaxi/android_demo/LoginTest.java
@@ -1,7 +1,6 @@
-package com.mytaxi.android_demo.activities;
+package com.mytaxi.android_demo;
 
 
-import android.support.test.espresso.DataInteraction;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -10,38 +9,36 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 
-import com.mytaxi.android_demo.R;
+import com.mytaxi.android_demo.activities.MainActivity;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
-import org.hamcrest.core.IsInstanceOf;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.replaceText;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.anything;
 import static org.hamcrest.Matchers.is;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class TestActivity1 {
+public class LoginTest {
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void testActivity1() {
+    public void loginTest() {
         // Added a sleep statement to match the app's execution delay.
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
@@ -51,32 +48,58 @@ public class TestActivity1 {
             e.printStackTrace();
         }
 
-        ViewInteraction appCompatAutoCompleteTextView = onView(
-                allOf(withId(R.id.textSearch),
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.edt_username),
                         childAtPosition(
-                                allOf(withId(R.id.searchContainer),
-                                        childAtPosition(
-                                                withClassName(is("android.support.design.widget.CoordinatorLayout")),
-                                                1)),
+                                childAtPosition(
+                                        withClassName(is("android.support.design.widget.TextInputLayout")),
+                                        0),
                                 0),
                         isDisplayed()));
-        appCompatAutoCompleteTextView.perform(replaceText("sa"), closeSoftKeyboard());
+        appCompatEditText.perform(replaceText("whiteelephant261"), closeSoftKeyboard());
 
-        DataInteraction appCompatTextView = onData(anything())
-                .inAdapterView(childAtPosition(
-                        withClassName(is("android.widget.PopupWindow$PopupBackgroundView")),
-                        0))
-                .atPosition(2);
-        appCompatTextView.perform(click());
-
-        ViewInteraction viewGroup = onView(
-                allOf(childAtPosition(
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.edt_password),
                         childAtPosition(
-                                IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class),
+                                childAtPosition(
+                                        withClassName(is("android.support.design.widget.TextInputLayout")),
+                                        0),
                                 0),
-                        0),
                         isDisplayed()));
-        viewGroup.check(matches(isDisplayed()));
+        appCompatEditText2.perform(replaceText("video"), closeSoftKeyboard());
+
+        pressBack();
+
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.btn_login), withText("Login"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                2),
+                        isDisplayed()));
+        appCompatButton.perform(click());
+
+//        ViewInteraction appCompatImageButton = onView(
+//                allOf(withContentDescription("Open navigation drawer"),
+//                        childAtPosition(
+//                                allOf(withId(R.id.toolbar),
+//                                        childAtPosition(
+//                                                withClassName(is("android.support.design.widget.AppBarLayout")),
+//                                                0)),
+//                                1),
+//                        isDisplayed()));
+//        appCompatImageButton.perform(click());
+//
+//        ViewInteraction textView = onView(
+//                allOf(withId(R.id.nav_username), withText("whiteelephant261"),
+//                        childAtPosition(
+//                                childAtPosition(
+//                                        withId(R.id.navigation_header_container),
+//                                        0),
+//                                1),
+//                        isDisplayed()));
+//        textView.check(matches(isDisplayed()));
 
     }
 
---
+++ app/src/androidTest/java/com/mytaxi/android_demo/LogoutTest.java
@@ -0,0 +1,139 @@
+package com.mytaxi.android_demo;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import com.mytaxi.android_demo.activities.MainActivity;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class LogoutTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void logoutTest() {
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction relativeLayout = onView(
+                allOf(childAtPosition(
+                        childAtPosition(
+                                withId(R.id.map),
+                                0),
+                        0),
+                        isDisplayed()));
+        relativeLayout.check(matches(isDisplayed()));
+
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withContentDescription("Open navigation drawer"),
+                        childAtPosition(
+                                allOf(withId(R.id.toolbar),
+                                        childAtPosition(
+                                                withClassName(is("android.support.design.widget.AppBarLayout")),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
+        ViewInteraction linearLayout = onView(
+                allOf(childAtPosition(
+                        allOf(withId(R.id.navigation_header_container),
+                                childAtPosition(
+                                        withId(R.id.design_navigation_view),
+                                        0)),
+                        0),
+                        isDisplayed()));
+        linearLayout.check(matches(isDisplayed()));
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.nav_username), withText("whiteelephant261"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.navigation_header_container),
+                                        0),
+                                1),
+                        isDisplayed()));
+        textView.check(matches(withText("whiteelephant261")));
+
+        ViewInteraction checkedTextView = onView(
+                allOf(withId(R.id.design_menu_item_text),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.design_navigation_view),
+                                        1),
+                                0),
+                        isDisplayed()));
+        checkedTextView.check(matches(isDisplayed()));
+
+        ViewInteraction navigationMenuItemView = onView(
+                allOf(childAtPosition(
+                        allOf(withId(R.id.design_navigation_view),
+                                childAtPosition(
+                                        withId(R.id.nav_view),
+                                        0)),
+                        1),
+                        isDisplayed()));
+        navigationMenuItemView.perform(click());
+
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.edt_username),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.support.design.widget.TextInputLayout")),
+                                        0),
+                                0),
+                        isDisplayed()));
+        appCompatEditText.perform(click());
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
